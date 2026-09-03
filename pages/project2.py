80d405faf48fdbba50f3f6e818e8963509557852
Michael <mymis168@gmail.com>
2026-09-03 10:19:13 +0800

    menulist 版本

---
+++ pages/project2.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.title("專案首頁2")
--- streamlit_main2.py
+++ streamlit_main2.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 pageList=[
-    st.Page("pages/home.py" , title="Home"),
+    st.Page("pages/home.py" , title="Home" ),
     st.Page("pages/mycv.py" , title="我的履歷" ),   
     st.Page("pages/myproject.py" , title="執行專案")    
 ]
